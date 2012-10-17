42ab7aab52b0f5218230ce5705f0d2897ceebb23
SM <dervishx@stu.aii.edu>
2012-10-16 17:10:58 -0700

    formatting

--- wxRaytracer/raytracer/Utilities/Ray.cpp
+++ wxRaytracer/raytracer/Utilities/Ray.cpp
@@ -4,35 +4,35 @@
 
 Ray::Ray (void)
 	: 	o(0.0), 
-		d(0.0, 0.0, 1.0) 
+	d(0.0, 0.0, 1.0) 
 {}
 
 // ---------------------------------------------------------------- constructor
 
 Ray::Ray (const Point3D& origin, const Vector3D& dir)
 	: 	o(origin), 
-		d(dir) 
+	d(dir) 
 {}
 
 // ---------------------------------------------------------------- copy constructor
 
 Ray::Ray (const Ray& ray)
 	: 	o(ray.o), 
-		d(ray.d) 
+	d(ray.d) 
 {}
 
 // ---------------------------------------------------------------- assignment operator
 
 Ray& 
-Ray::operator= (const Ray& rhs) {
-	
-	if (this == &rhs)
-		return (*this);
-		
-	o = rhs.o; 
-	d = rhs.d; 
+	Ray::operator= (const Ray& rhs) {
 
-	return (*this);	
+		if (this == &rhs)
+			return (*this);
+
+		o = rhs.o; 
+		d = rhs.d; 
+
+		return (*this);	
 }
 
 // ---------------------------------------------------------------- destructor
